cf8b61d373eaec3f5c4f3456232aab6dc5a5ee78
ndornseif <5382403+ndornseif@users.noreply.github.com>
2023-04-04 11:04:07 +0200

    File upload

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -1,31 +1,36 @@
-//Make sure StartPin has a higher pin number that EndPin.
-#define CBUSStartPin 9 //First bit of the counter outputs. Should be connected to Y0.
-//The next lower seven pins are the remainig CBUS pins. E.g. if CBUSStartPin is 9, connect Y0 to pin 9 and Y7 to pin 2
+//MSM501-MainsReciprocalCounter Firmware version 1.6
 
-#define CounterClear A0 //Pin connected to CCLR# on both counters.
-#define CounterRegClock A2 //Pin connected to RCLK on both counters.
-#define GateTime A1 //Pin used to open the main gate and pass the signal and reference to the counters
+//Pin definitions
+//Counter output connections starting at Y0.
+const byte CBUS[8] = {9, 8, 7, 6, 5, 4, 3, 2};
+
+const byte CounterClear = 14; //Pin connected to CCLR# on both counters.
+const byte CounterRegClock = 16; //Pin connected to RCLK on both counters.
+const byte GateTime = 15; //Pin used to open the main gate and pass the signal and reference to the counters
 
 //The SBUS is used to select which counter, as well as what byte of said counter will be read.
 //SBUS2 selects the counter to read. 0=SignalCounter; 1=ReferenceCounter
 //SBUS0 and SBUS1 are used to select one of the four bytes stored in each counter.
 //0=CounterA lower byte, 1=CounterA upper byte, 2=CounterB lower byte, 3=CounterB upper byte.
 //Pulling SBUSE low puts the outputs of both counters in HiZ mode. Leaves the CBUS floating. Pull high to read.
-#define SBUS0 10
-#define SBUS1 11
-#define SBUS2 12
-#define SBUSE 13
+const byte SBUS0 = 10;
+const byte SBUS1 = 11;
+const byte SBUS2 = 12;
+const byte SBUSE = 13;
 
-#define ReadGateStatus A3 //The pin connected to the gateEnable signal out of the flipflop. Used to sense if main gate is open.
+//Pin connected to the gateEnable signal out of the flipflop. Used to sense if main gate is open.
+const byte ReadGateStatus = 17; 
 
-#define StatusLED A4 //Red LED to indicate whatever. Connected to the positive terminal of the status LED.
+//LED to indicate whatever. Connected to the positive terminal of the status LED.
+const byte StatusLED = 18;
 
-#define ConditionedSignal A5 //The pin connected to the conditioned input signal. Used for general debugging.
+//Pin connected to the conditioned input signal. Currently unused.
+const byte ConditionedSignal = 19;
 
 //The frequency of the reference clock in uHz.
-const float refClockFrequency = 1000000000000;
+const float refClockFrequency = 1e6 * 1e6; //1MHz multiplied by 1e6 to convert from Hz to uHz.
 
-//The time a single measurment is perfomed by default in ms.
+//Default time for a single measurment in ms.
 const unsigned long defaultGateTime = 60000;
 
 //Resets Reference and Signal Counters.
@@ -33,16 +38,18 @@ void resetCounters(){
   digitalWrite(CounterClear,LOW);
   digitalWrite(CounterClear,HIGH);
 }
+
 //Latches the Reference and Signal Counter states in their internal registers.
 void latchCounters(){
   digitalWrite(CounterRegClock,HIGH);
   digitalWrite(CounterRegClock,LOW);
 }
 
+//Read in the value represented on the CBUS.
 byte readCBUS(){
   byte returnvalue = 0;
   for(byte i = 0; i < 8; i++){
-    bitWrite(returnvalue,i,digitalRead(CBUSStartPin-i));
+    bitWrite(returnvalue,i,digitalRead(CBUS[i]));
   }
   return returnvalue;
 }
@@ -50,9 +57,12 @@ byte readCBUS(){
 //Read counter in. counterIndex=0 -> Signal Counter; counterIndex=1 -> ReferenceCounter
 unsigned long readCounter(bool counterIndex){
   unsigned long returnvalue = 0;
-  byte SBUSState = 0b1011; //Set bit for SBUS enable high and byte counter (two LSBs) to three.
-  bitWrite(SBUSState, 2, counterIndex);//Select the counter ro read from.
-  for(byte i = 0; i < 4;i++){ //Extract all four bytes of the counter one by one
+  //Set bit for SBUS enable high and byte counter to three (selecting upper byte of counter B).
+  byte SBUSState = 0b1011;
+  //Select the counter ro read from.
+  bitWrite(SBUSState, 2, counterIndex);
+  //Extract all four bytes of the counter one by one
+  for(byte i = 0; i < 4;i++){
     setSBUS(SBUSState-i);
     returnvalue = returnvalue << 8;
     returnvalue += readCBUS();
@@ -71,17 +81,11 @@ void setSBUS(byte index){
 //Measures the Frequency coming in averaged over gateTime (in ms) returns value in uHz.
 unsigned long performMeasurement(unsigned long gateTime){
   resetCounters();
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
   digitalWrite(GateTime,HIGH);
   delay(gateTime);
   digitalWrite(GateTime,LOW);
-  delay(100);
-  latchCounters();
-  digitalWrite(LED_BUILTIN, HIGH);
   delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
+  latchCounters();
   float signalCounterValue = readCounter(0);
   float referenceCounterValue = readCounter(1);
   //All Frequency values are given in uHz.
@@ -90,18 +94,19 @@ unsigned long performMeasurement(unsigned long gateTime){
 }
 void setup() {
   Serial.begin(115200);
-  //CBUS pins are numbered in decending order.
-  for(byte i = 0; i < 8; i++){
-    pinMode(CBUSStartPin-i, INPUT);
-  }
-  pinMode(ReadGateStatus,INPUT);
-  pinMode(StatusLED,OUTPUT);
-  pinMode(ConditionedSignal,INPUT);
   
-  pinMode(LED_BUILTIN, OUTPUT);
+  for(byte i = 0; i < 8; i++){
+      pinMode(CBUS[i], INPUT);
+  }
+
+  pinMode(ReadGateStatus,INPUT);
+  pinMode(ConditionedSignal,INPUT);
+
+  pinMode(StatusLED,OUTPUT);
   pinMode(CounterClear,OUTPUT);
   pinMode(CounterRegClock,OUTPUT);
   pinMode(GateTime,OUTPUT);
+  
   pinMode(SBUS0,OUTPUT);
   pinMode(SBUS1,OUTPUT);
   pinMode(SBUS2,OUTPUT);
@@ -111,8 +116,7 @@ void setup() {
   digitalWrite(CounterClear,HIGH); //Dont clear counter.
   digitalWrite(CounterRegClock,LOW); //Dont latch the counter state.
   digitalWrite(GateTime,LOW); //Leave main gate closed.
-  analogWrite(StatusLED,0); //Status LED off.
-  digitalWrite(LED_BUILTIN,LOW); //Arduino genral purpose LED off.
+  digitalWrite(StatusLED,LOW); //Status LED off.
 
   resetCounters();
 }
--- README.md
+++ README.md
@@ -1,30 +1,36 @@
 # MSM501-MainsReciprocalCounter
-Measures the frequency of mains power.
+Measures the frequency of mains power. <br>
+Hardware version: 1.1
+Firmware version: 1.6
 
 
-Test points:
-TP1 - Offset voltage to compensate for clipping diode drop. (~500mV DC)
-TP2 - Divided and clipped transformer output voltage. (~4Vp half wave rectified sine)
-TP3 - Unregulated rectified and smoothed transformer voltage. (>17V DC)
-TP4 - DC Supply rails. (+5V and +3.3V DC)
-TP5 - Reference oscillator signal. (1MHz or 10MHz 0-5V square wave)
-TP6 - Gate-Enable signal from MCU. High if measurement is about to start or running.
-TP7 - Input to the signal counter.
-TP8 - Input to the reference counter.
-TP9 - Transformer output voltage to measurement circuit. (>12V AC)
-TP10 - External reference clock input.
-TP11 - Reference input to the main gate. Should show same signal as TP10 or TP5 depending on JMP2 (Clock select).
-TP12 - Signal input to the main gate. (0-5V square wave with same frequency as mains)
-
-Jumpers:
-JMP1 (J3) - Selects if measured signal is inverted. This should not affect the measurement. Recommend setting to INV not BUF. 
-JMP2 (J4) - Selects external or internal reference clock. Check TP11 for selected signal.
-
-Connectors and Headers:
-GND - Ground connection for scope ground clips.
-TP4 - DC voltage rails.
-J2 - Serial interface. Connected to D0/D1 of arduino. (5V max.)
-J5 - Debug pins. (SBUS0, SBUS1, SBUS2, SBUSE, CounterClear, GateTime, CounterRegClock, Reset)
-J6 - CBUS. Used to read out the counter values.
-CN1 - Mains input. Fuse with 5x20mm 32mA glass fuse in F1.
-CN2 - External refernece input. BNC. 0-5V square wave only. HiZ input.
+## Overview
+This device was built as an exercise in digital electronics. It uses a reciprocal frequency counter approach to measure the frequency of the mains voltage supplying it. <br>
+Using two SN74LV8154N 32bit counters, it counts pulses of the mains signal and the reference clock. The start and end of measurement are syncronized to the input signal using a D-FlipFlop to open the main gate. 
+This eliminates the influence of gate time on the measurement resolution.<br>
+The measurement result is then calculated as follows: <br>
+$measuredFreq = (signalCounterValue * referenceClockFrequency) / referenceCounterValue$
+## Connectors/Headers
+- GND: Ground connection for scope ground clips.
+- TP4: DC voltage rails.
+- J2: Serial interface. Connected to D0/D1 of Arduino. (5V max.)
+- J5: Debug pins. (SBUS0, SBUS1, SBUS2, SBUSE, CounterClear, GateTime, CounterRegClock, Reset)
+- J6: CBUS. Used to read out the counter values.
+- CN1: Mains input. Fuse with 5x20mm 32mA glass fuse in F1.
+- CN2: External reference input. BNC. 0-5V square wave only. HiZ input.
+## Jumpers
+- JMP1 (J3): Selects if measured signal is inverted. This should not affect the measurement. Recommended setting is INV.
+- JMP2 (J4): Selects external or internal reference clock. Check TP11 for selected signal.
+## Test points
+- TP1: Offset voltage to compensate for the clipping diode drop. (~ 500mV DC)
+- TP2: Divided and clipped transformer output voltage. (~ 4V~p~ half wave rectified sine)
+- TP3: Unregulated rectified and smoothed transformer voltage. (> 17V DC)
+- TP4: DC Supply rails. (+5V and +3.3V DC)
+- TP5: Reference oscillator signal. (1MHz or 10MHz 0-5V square wave)
+- TP6: Gate-Enable signal from FlipFlop. High if main gate is open and measurement running.
+- TP7: Input to the signal counter.
+- TP8: Input to the reference counter.
+- TP9: Transformer output voltage to measurement circuit. (> 12V AC)
+- TP10: External reference clock input.
+- TP11: Reference input to the main gate. Should show the same signal as TP10 or TP5 depending on JMP2 (Clock select).
+- TP12: Signal input to the main gate. (0-5V square wave with same frequency as mains)
